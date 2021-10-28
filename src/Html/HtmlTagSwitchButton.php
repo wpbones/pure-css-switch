bd2ed83ca81dc2b02fd06dcd0022279204066e63
John Fazioli <john.fazioli@namecheap.com>
2021-10-28 15:00:09 +0200

    updates gulp

--- src/Html/HtmlTagSwitchButton.php
+++ src/Html/HtmlTagSwitchButton.php
@@ -4,7 +4,7 @@ namespace WPKirk\PureCSSSwitch\Html;
 
 use WPKirk\WPBones\Html\HtmlTag;
 
-if ( ! function_exists( 'wpbones_is_true' ) ) {
+if (! function_exists('wpbones_is_true')) {
 
   /**
    * Utility to check if a value is true.
@@ -13,22 +13,22 @@ if ( ! function_exists( 'wpbones_is_true' ) ) {
    *
    * @return bool
    */
-  function wpbones_is_true( $value )
-  {
-    return ! in_array( strtolower( $value ), [ '', 'false', '0', 'no', 'n', 'off', null ] );
-  }
+    function wpbones_is_true($value)
+    {
+        return ! in_array(strtolower($value), [ '', 'false', '0', 'no', 'n', 'off', null ]);
+    }
 }
 
 class HtmlTagSwitchButton extends HtmlTag
 {
-  protected $attributes = [
+    protected $attributes = [
     'name'  => null,
     'id'    => null,
     'value' => '1',
     'class' => ''
   ];
 
-  protected $guardedAttributes = [
+    protected $guardedAttributes = [
     'theme'       => 'flat-round',
     'right_label' => null,
     'left_label'  => null,
@@ -37,58 +37,56 @@ class HtmlTagSwitchButton extends HtmlTag
     'mode'        => 'switch'
   ];
 
-  public static function __callStatic( $name, $arguments )
-  {
-    $args = ( isset( $arguments[ 0 ] ) && ! is_null( $arguments[ 0 ] ) ) ? $arguments[ 0 ] : [];
+    public static function __callStatic($name, $arguments)
+    {
+        $args = (isset($arguments[ 0 ]) && ! is_null($arguments[ 0 ])) ? $arguments[ 0 ] : [];
 
-    $instance = new self();
+        $instance = new self();
 
-    return $instance->{$name}( $args );
-  }
-
-  public function html()
-  {
-    if ( empty( $this->id ) ) {
-      $this->id = $this->name;
+        return $instance->{$name}($args);
     }
 
-    if ( empty( $this->name ) ) {
-      $this->name = $this->id;
-    }
+    public function html()
+    {
+        if (empty($this->id)) {
+            $this->id = $this->name;
+        }
 
-    $mode = 'switch';
-    if( in_array( $this->mode, [ 'switch', 'select' ] ) ) {
-      $mode = $this->mode;
-    }
+        if (empty($this->name)) {
+            $this->name = $this->id;
+        }
 
-    $checked    = wpbones_is_true( $this->checked ) ? 'checked="checked"' : '';
-    $disabled   = wpbones_is_true( $this->disabled ) ? 'disabled="disabled"' : '';
-    $leftLabel  = wpbones_is_true( $this->left_label ) ? "<label for=\"{$this->id}\">{$this->left_label}</label>" : '';
-    $rightLabel = wpbones_is_true( $this->right_label ) ? "<label for=\"{$this->id}\">{$this->right_label}</label>" : '';
+        $mode = 'switch';
+        if (in_array($this->mode, [ 'switch', 'select' ])) {
+            $mode = $this->mode;
+        }
 
-    ob_start(); ?>
+        $checked    = wpbones_is_true($this->checked) ? 'checked="checked"' : '';
+        $disabled   = wpbones_is_true($this->disabled) ? 'disabled="disabled"' : '';
+        $leftLabel  = wpbones_is_true($this->left_label) ? "<label for=\"{$this->id}\">{$this->left_label}</label>" : '';
+        $rightLabel = wpbones_is_true($this->right_label) ? "<label for=\"{$this->id}\">{$this->right_label}</label>" : '';
 
-    <div class="wpbones-switch-button wpbones-switch-mode-<?php echo $mode ?> wpbones-switch-button-<?php echo $this->theme ?> <?php echo $disabled ? 'disabled' : '' ?>">
-      <input type="hidden"
-             name="<?php echo $this->name ?>"
-             value="0"/>
-      <?php echo $leftLabel ?>
-      <input
-        <?php echo $this->formatDataAttributes() ?>
-        <?php echo $this->formatAttributes() ?>
-        <?php echo $this->formatGlobalAttributes() ?>
-          type="checkbox"
-        <?php echo $checked ?>
-        <?php echo $disabled ?>
-          value="<?php echo $this->value ?>"/>
-      <label for="<?php echo $this->id ?>"></label>
-      <?php echo $rightLabel ?>
-    </div>
+        ob_start(); ?>
 
-    <?php
+<div
+  class="wpbones-switch-button wpbones-switch-mode-<?php echo $mode ?> wpbones-switch-button-<?php echo $this->theme ?> <?php echo $disabled ? 'disabled' : '' ?>">
+  <input type="hidden" name="<?php echo $this->name ?>" value="0" />
+  <?php echo $leftLabel ?>
+  <input <?php echo $this->formatDataAttributes() ?>
+  <?php echo $this->formatAttributes() ?>
+  <?php echo $this->formatGlobalAttributes() ?>
+  type="checkbox"
+  <?php echo $checked ?>
+  <?php echo $disabled ?>
+  value="<?php echo $this->value ?>"/>
+  <label for="<?php echo $this->id ?>"></label>
+  <?php echo $rightLabel ?>
+</div>
+
+<?php
     $html = ob_get_contents();
-    ob_end_clean();
+        ob_end_clean();
 
-    return $html;
-  }
+        return $html;
+    }
 }
--- src/PureCSSSwitchProvider.php
+++ src/PureCSSSwitchProvider.php
@@ -4,24 +4,26 @@ namespace WPKirk\PureCSSSwitch;
 
 class PureCSSSwitchProvider
 {
+    public static function css($minified = true)
+    {
+        $file = __FILE__;
 
-  public static function css( $minified = true )
-  {
-    $file = __FILE__;
+        $path = rtrim(plugin_dir_url($file), '\/');
 
-    $path = rtrim( plugin_dir_url( $file ), '\/' );
+        $minified = $minified ? ".min" : "";
 
-    $minified = $minified ? ".min" : "";
+        $css = "{$path}/public/css/wpbones-switch-theme{$minified}.css";
 
-    $css = "{$path}/public/css/wpbones-switch-theme{$minified}.css";
+        return $css;
+    }
 
-    return $css;
-  }
-
-  public static function enqueueStyles( $minified = true )
-  {
-    wp_enqueue_style( 'wpbones-switch-theme', self::css( $minified ),
-                      [],
-                      WPKirk()->Version );
-  }
+    public static function enqueueStyles($minified = true)
+    {
+        wp_enqueue_style(
+            'wpbones-switch-theme',
+            self::css($minified),
+            [],
+            WPKirk()->Version
+        );
+    }
 }
