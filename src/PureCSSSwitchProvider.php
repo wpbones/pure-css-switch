14c9b060b3cc8bc1d3ac86a3d73d2f42f94afd83
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-05-21 16:45:55 +0200

    🔖 1.2

--- src/PureCSSSwitchProvider.php
+++ src/PureCSSSwitchProvider.php
@@ -4,26 +4,23 @@ namespace WPKirk\PureCSSSwitch;
 
 class PureCSSSwitchProvider
 {
-    public static function css($minified = true)
-    {
-        $file = __FILE__;
+  public static function css($minified = true)
+  {
+    $file = __FILE__;
+    $path = rtrim(plugin_dir_url($file), '\/');
+    $minified = $minified ? ".min" : "";
+    $css = "{$path}/public/css/wpbones-switch-theme{$minified}.css";
 
-        $path = rtrim(plugin_dir_url($file), '\/');
+    return $css;
+  }
 
-        $minified = $minified ? ".min" : "";
-
-        $css = "{$path}/public/css/wpbones-switch-theme{$minified}.css";
-
-        return $css;
-    }
-
-    public static function enqueueStyles($minified = true)
-    {
-        wp_enqueue_style(
-            'wpbones-switch-theme',
-            self::css($minified),
-            [],
-            WPKirk()->Version
-        );
-    }
+  public static function enqueueStyles($minified = true)
+  {
+    wp_enqueue_style(
+      'wpbones-switch-theme',
+      self::css($minified),
+      [],
+      WPKirk()->Version
+    );
+  }
 }
